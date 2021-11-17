4389dbb0bd4d0e3aae8f9c758bf29bcde356bc77
ertaig <82403563+ertaig@users.noreply.github.com>
2021-11-17 18:29:48 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('.header__burger').click(function(){
+        $('.header__burger').toggleClass('open-menu');
+        $('.header__menu').toggleClass('open-menu');
+	});
+});
